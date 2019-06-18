16e174a7c5fc054f0fbfc6b51277497b7bdc03cb
izzyus <bogdan.catalin.grigore@gmail.com>
2019-06-18 10:16:15 +0300

    Archive Manager will temporary uses hardcoded archives

--- WoWExport/WoWExport/Managers/ArchiveManager.cs
+++ WoWExport/WoWExport/Managers/ArchiveManager.cs
@@ -11,6 +11,12 @@ namespace Managers
     {
         public static List<String> MainListFile = new List<String>();
         public static String GameDir;
+        public static MpqArchive art = new MpqArchive(@"D:\World of Warcraft - Cataclysm\Data\art.mpq", FileAccess.Read);
+        public static MpqArchive expansion1 = new MpqArchive(@"D:\World of Warcraft - Cataclysm\Data\expansion1.MPQ", FileAccess.Read);
+        public static MpqArchive expansion2 = new MpqArchive(@"D:\World of Warcraft - Cataclysm\Data\expansion2.MPQ", FileAccess.Read);
+        public static MpqArchive expansion3 = new MpqArchive(@"D:\World of Warcraft - Cataclysm\Data\expansion3.MPQ", FileAccess.Read);
+        public static MpqArchive world = new MpqArchive(@"D:\World of Warcraft - Cataclysm\Data\world.mpq", FileAccess.Read);
+        public static MpqArchive world2 = new MpqArchive(@"D:\World of Warcraft - Cataclysm\Data\world2.mpq", FileAccess.Read);
 
         public static void GenerateMainListFileFromTXT()
         {
@@ -62,7 +68,8 @@ namespace Managers
         {
             //Find the goddamn file in the archive hell
             int index = MainListFile.FindIndex(a => a.Contains(filename.ToLower()));
-            Stream stream;
+            //Stream stream;
+            Stream stream = null;
 
             if (index != -1)
             {
@@ -70,12 +77,14 @@ namespace Managers
                 string archive = MainListFile[index];
                 archive = archive.Substring(archive.LastIndexOf(';', archive.Length - 2) + 1);
 
+                //Old manager:
+                /*
                 //i just realised how obsolete this one is... (string filename) <-- seems familiar?
                 //Get the file (+path) 
-                /*
-                string file = MainListFile[index];
-                file = file.Substring(0, file.LastIndexOf(';'));
-                */
+                
+                //string file = MainListFile[index];
+                //file = file.Substring(0, file.LastIndexOf(';'));
+                
 
                 //Try to open the archive and read the requested file
                 try
@@ -87,6 +96,29 @@ namespace Managers
                 {
                     //Error while opening the archive / reading the file
                     stream = null;
+                }
+                */
+
+                switch (archive.Substring(0, archive.Length - 4))
+                {
+                    case "art":
+                        stream = art.OpenFile(filename);
+                        break;
+                    case "expansion1":
+                        stream = expansion1.OpenFile(filename);
+                        break;
+                    case "expansion2":
+                        stream = expansion2.OpenFile(filename);
+                        break;
+                    case "expansion3":
+                        stream = expansion3.OpenFile(filename);
+                        break;
+                    case "world":
+                        stream = world.OpenFile(filename);
+                        break;
+                    case "world2":
+                        stream = world2.OpenFile(filename);
+                        break;
                 }
             }
             else
@@ -113,7 +145,7 @@ namespace Managers
                         listFile = sr.ReadToEnd();
                         Console.WriteLine(listFile);
                     }
-                    archive.ExtractFile("(listfile)", to + "\\" + fileinfo.Name.Replace(".MPQ",".txt"));
+                    archive.ExtractFile("(listfile)", to + "\\" + fileinfo.Name.Replace(".MPQ", ".txt"));
                 }
             }
         }
@@ -131,5 +163,6 @@ namespace Managers
             }
             return exists;
         }
+
     }
 }
