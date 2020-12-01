474a6603f199b7e96c8f92f28738569d006ee0b9
izzyus <bogdan.catalin.grigore@gmail.com>
2020-12-01 09:34:42 +0200

    Experimental Local DBCD Provider

---
+++ WoWExport/WoWExport/DBC/ArchiveDBCProvider.cs
@@ -0,0 +1,20 @@
+﻿using DBCD.Providers;
+using System.IO;
+
+namespace WoWExport.DBC
+{
+    class ArchiveDBCProvider : IDBCProvider
+    {
+        public Stream StreamForTableName(string tableName, string build)
+        {
+            if(Managers.ArchiveManager.FileExists("dbfilesclient/" + tableName + ".db2"))
+            {
+                return Managers.ArchiveManager.ReadThisFile("dbfilesclient/" + tableName + ".db2");
+            }
+            else
+            {
+                throw new FileNotFoundException("DBC " + tableName + " not found in the archive!");
+            }
+        }
+    }
+}
---
+++ WoWExport/WoWExport/DBC/LocalDBCDProvider.cs
@@ -0,0 +1,22 @@
+﻿using DBCD.Providers;
+using System.IO;
+
+namespace WoWExport.DBC
+{
+    class LocalDBCDProvider : IDBDProvider
+    {
+        string localDefinitionPath = @"D:/test/definitions"; //HARDCODED FOR THE MOMENT
+
+        public Stream StreamForTableName(string tableName, string build = null)
+        {
+            if(File.Exists(localDefinitionPath + "/" + tableName + ".dbd"))
+            {
+                return File.OpenRead(localDefinitionPath + "/" + tableName + ".dbd");
+            }
+            else
+            {
+                throw new FileNotFoundException("Definition " + tableName + " not found in " + localDefinitionPath);
+            }
+        }
+    }
+}
