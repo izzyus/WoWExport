d84551493c826d77f911589cb4f0c30596590648
izzyus <bogdan.catalin.grigore@gmail.com>
2021-01-23 09:29:00 +0200

    Download needed DBC Definitions if not provided by user

--- WoWExport/WoWExport/DBC/LocalDBCDProvider.cs
+++ WoWExport/WoWExport/DBC/LocalDBCDProvider.cs
@@ -1,5 +1,7 @@
 ﻿using DBCD.Providers;
 using System.IO;
+using System.IO.Compression;
+using System.Net;
 
 namespace WoWExport.DBC
 {
@@ -13,7 +15,27 @@ namespace WoWExport.DBC
             }
             else
             {
-                throw new FileNotFoundException("Definition " + tableName + " not found in " + Managers.ConfigurationManager.LocalDBCDefinitionLoc);
+                //throw new FileNotFoundException("Definition " + tableName + " not found in " + Managers.ConfigurationManager.LocalDBCDefinitionLoc);
+                try
+                {
+                    using (var client = new WebClient())
+                    using (var stream = new MemoryStream())
+                    {
+                        client.Headers[HttpRequestHeader.AcceptEncoding] = "gzip";
+                        var responseStream = new GZipStream(client.OpenRead($"https://raw.githubusercontent.com/wowdev/WoWDBDefs/master/definitions/{tableName}.dbd"), CompressionMode.Decompress);
+                        responseStream.CopyTo(stream);
+
+                        File.WriteAllBytes(Path.Combine(Managers.ConfigurationManager.LocalDBCDefinitionLoc + "/" + tableName + ".dbd"), stream.ToArray());
+
+                        responseStream.Close();
+                        responseStream.Dispose();
+                    }
+                    return File.OpenRead(Managers.ConfigurationManager.LocalDBCDefinitionLoc + "/" + tableName + ".dbd");
+                }
+                catch
+                {
+                    throw new FileNotFoundException("Definition " + tableName + " not found in " + Managers.ConfigurationManager.LocalDBCDefinitionLoc + " and it couldn't be downloaded either");
+                }
             }
         }
     }
